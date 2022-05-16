8b36d3220b4e5af3959ca218f0572427b0f35d1c
rezaul11703 <rezaulkarim11703@gmail.com>
2022-05-16 22:12:01 +0600

    google log in page completed with errors and loading

--- src/Pages/LogIn/LogIn.js
+++ src/Pages/LogIn/LogIn.js
@@ -1,21 +1,112 @@
 import React from "react";
 import {
+  useSignInWithEmailAndPassword,
   useSignInWithGoogle,
   useSignInWithXXX,
 } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
-
+import { useForm } from "react-hook-form";
+import Loading from "../Shared/Loading";
 const LogIn = () => {
-  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
-  if (user) {
-    console.log(user);
+  const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  let signInError;
+  // if true|| loading it will loading all the time
+  if (loading || gLoading) {
+    return <Loading></Loading>;
   }
+  if (error || gError) {
+    signInError = <p>{error?.message || gError?.message}</p>;
+  }
+  if (gUser) {
+    console.log(gUser);
+  }
+
+  const onSubmit = (data) => {
+    signInWithEmailAndPassword(data.email, data.password);
+  };
+
   return (
-    <div className="flex justify-center align-center">
+    <div className="flex justify-center align-center h-screen ">
       <div className="card w-96 bg-base-100 shadow-xl">
         <div className="card-body">
           <h2 className="text-center text-bold text-primary tex-xl">Log In</h2>
         </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div class="form-control w-full max-w-xs">
+            <label class="label">
+              <span class="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="Enter Your Email Here"
+              class="input input-bordered w-full max-w-xs"
+              {...register("email", {
+                required: {
+                  value: true,
+                  message: "please Input a Valid Email",
+                },
+                pattern: {
+                  value: /[a-z0-9]+@[a-z]+.[a-z]{2,3}/,
+                  message: "Provide a valid Email",
+                },
+              })}
+            />
+            <label class="label">
+              {errors.email?.type === "required" && (
+                <span class="label-text-alt text-red">
+                  {errors.email.message}
+                </span>
+              )}
+              {errors.email?.type === "pattern" && (
+                <span class="label-text-alt text-red-500">
+                  {errors.email.message}
+                </span>
+              )}
+            </label>
+          </div>
+          <div class="form-control w-full max-w-xs">
+            <label class="label">
+              <span class="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Your Password Here"
+              class="input input-bordered w-full max-w-xs"
+              {...register("password", {
+                required: {
+                  value: true,
+                  message: "please Input a Valid passward",
+                },
+                minLength: {
+                  value: 6,
+                  message: "Provided  Character of Six length",
+                },
+              })}
+            />
+            <label class="label">
+              {errors.password?.type === "required" && (
+                <span class="label-text-alt text-red">
+                  {errors.password.message}
+                </span>
+              )}
+              {errors.password?.type === "minLength" && (
+                <span class="label-text-alt text-red-500">
+                  {errors.password.message}
+                </span>
+              )}
+            </label>
+          </div>
+          {signInError}
+          <input className="btn w-full max-w-xs" type="submit" value="login" />
+        </form>
+
         <div className="divider">OR</div>
         <button className="btn btn-outline" onClick={() => signInWithGoogle()}>
           Sign in with Google
